f47ec25c10dc75a6f106c58ec847e8183f593443
Scott Kao <scottxxx666@gmail.com>
2025-05-10 21:43:52 +0800

    feat: use google gemini api

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,14 +1,16 @@
 import { openai } from "@ai-sdk/openai";
+import { google } from "@ai-sdk/google";
 import { jsonSchema, streamText } from "ai";
 
 export const runtime = "edge";
 export const maxDuration = 30;
 
 export async function POST(req: Request) {
-  const { messages, system, tools } = await req.json();
+  const {messages, system, tools, model: modelText} = await req.json();
 
+  const model = modelText == "gemini-2.5" ? openai(modelText) : google("gemini-2.5-flash-preview-04-17");
   const result = streamText({
-    model: openai("gpt-4o"),
+    model: model,
     messages,
     // forward system prompt and tools from the frontend
     system,
