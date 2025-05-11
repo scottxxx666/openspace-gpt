faafb3930a314726e52eb93d0f225b6564ffb587
Scott Kao <scottxxx666@gmail.com>
2025-05-11 23:09:31 +0800

    feat: add xai grok

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,6 +1,7 @@
 import { openai } from "@ai-sdk/openai";
 import { google } from "@ai-sdk/google";
 import { jsonSchema, streamText } from "ai";
+import { xai } from "@ai-sdk/xai";
 
 export const runtime = "edge";
 export const maxDuration = 30;
@@ -8,7 +9,7 @@ export const maxDuration = 30;
 export async function POST(req: Request) {
   const {messages, system, tools, model: modelText} = await req.json();
 
-  const model = modelText == "gemini-2.5" ? openai(modelText) : google("gemini-2.5-flash-preview-04-17");
+  const model = modelText == "gemini-2.5" ? google("gemini-2.5-flash-preview-04-17") : xai("grok-3-mini-beta");
   const result = streamText({
     model: model,
     messages,
